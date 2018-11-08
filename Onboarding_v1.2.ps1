5505788e266f2fabdf358b7a5d448f0c77c63efd
Sherman418 <32655155+Sherman418@users.noreply.github.com>
2018-11-08 10:37:58 -0500

    Update and rename Onboarding_v1.1 to Onboarding_v1.2.ps1

--- Onboarding_v1.2.ps1
+++ Onboarding_v1.2.ps1
@@ -1,17 +1,18 @@
 <#
-This script performs the following:
+	.ONBOARDING SCRIPT
+	
+	.FUNCTIONALITY
+	This script performs the following:
 	*Creates AD account with the format: [first initial][last name]
-		**If username already taken in AD, adds a "2" to the end of it, IE: JSmith2 in the event there are x2 John Smith users
+		**If username already taken in AD, adds a "2" to the end of it, IE: JSmith2 in the event there are x2 John Smith users.
+		**This acts as a loop and continues until it finds an available username, IE: JSmith3, JSmith4, etc.
 	*Sets a temporary password for that account
 	*Fills in all attributes of a user by department
 	*Creates a new home drive for that user
 	*Adds that user to all necessary security groups for their department
 	*Asks if this user needs an email address, and if so, creates an O365 account and assigns it an O365 Business Premium license
 		**Adds that user to all necessary email distribution lists and O365 groups for that dept
-	
-	Future Plans:
-	*Implement a better system for assigning usernames in the event that username already exists.  
-		**As it stands now, if there are 2 John Smiths it will try to create a second JSmith2.  Not a problem for smaller organizations but could be later on.
+
 #>
 
 Import-Module ActiveDirectory
@@ -25,9 +26,23 @@ $lastName = Read-Host -Prompt 'Enter last name '
 $fullName = $firstName + " " + $lastName
 $userName = $firstName.Substring(0,1)+$lastname
 
-$userExists = ([ADSISearcher] "(sAMAccountName=$userName)").FindOne()
-if ($userExists -ne $null) {
-    $userName = $userName + '2'
+<#
+    USERNAME CHECK
+    The loop below checks if $userName already exists in AD.
+    If it does exist, $userExist has a value of TRUE and initiates a loop.
+    The loop adds a "1" to the end of $userName.
+    It then performs a second check to see if $userName + 1 exists in AD.
+    If that name is still taken, the number is taken off the end of $userName, and the loop repeats with "2" appended.
+#>
+$userSuffix = 1
+$userExists = [bool] (Get-ADUser -Filter { SamAccountName -eq $userName })
+while ($userExists -eq $true) {
+    $userName = $userName + $userSuffix
+    $userSuffix++
+    $userExists = [bool] (Get-ADUser -Filter { SamAccountName -eq $userName })
+    if ($userExists -eq $true) {
+        $userName = $userName -replace ".$" #This removes the last character of $userName (the number.) Otherwise you end up with "$userName" + 12 instead of "$userName" + 2.
+    }
 }
 
 $password = "Password123" | ConvertTo-SecureString -AsPlainText -Force # Would recommend a more secure initial password...
